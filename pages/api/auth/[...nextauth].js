66b48d070f907a9ba0ffe5e39d169eed14bcc028
Vincent Valenzuela <vincent.valenzuela@live.com>
2022-06-14 12:05:03 -0400

    Implement GoogleProvider auth for web

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,9 @@ import GoogleProvider from "next-auth/providers/google"
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
