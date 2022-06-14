a29a47b42c9ffbf2bd666f192be2a030677a73ea
milkays <daveziesm@gmail.com>
2022-06-13 23:49:12 -0400

    [DAT-7-1] Navbar implementation

---
+++ components/HeaderLink.js
@@ -0,0 +1,9 @@
+function HeaderLink({Icon,text, avatar}){
+    return (
+        <div > 
+            {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1"/> : <Icon />}
+            <h4 className="text-sm">{text}</h4>
+        </div>
+    );
+}
+export default HeaderLink;
--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,26 @@
 import Image from "next/image";
 import logo from '../media/Logo.png'
+import HeaderLink from "../components/HeaderLink";
+import ExploreIcon from "@mui/icons-material/Explore";
+import GroupIcon from "@mui/icons-material/Group";
+import OndemandVideoSharpIcon from "@mui/icons-material/OndemandVideoSharp";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 function Home() {
   return (
     <div>
-      <header >
+      <header className="flex justify-around items-center py-4">
         <div className="relative w-36 h-10">
-              <Image src={logo} layout="fill" objectFit="contain" />
+        <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
             </div>
             <div className="flex items-center sm:divide-x divide-gray-300">
-              <div >hola</div>
-              
-              <div >mundo</div>
-            </div>
+              <div className="hidden sm:flex space-x-8 pr-4">
+              <HeaderLink Icon={ExploreIcon} text="Discover" />
+              <HeaderLink Icon={GroupIcon} text="People" />
+              <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
+              <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
+              </div>
+              </div>
        </header>
     </div> 
     );
