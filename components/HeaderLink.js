ff246d32951bd537d1acff1c1b6380621726a8da
paoli99 <pao_anahi_8@hotmail.com>
2022-06-15 16:07:56 -0400

    [DAT-10-6] added span in HeaderLink class

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -6,9 +6,14 @@ function HeaderLink({Icon,text, avatar, feed, active, hidden}){
         :"text-gray-500 hover:text-gray-700" 
          } ${active && "!text-black dark:!text-white"} `}> 
             {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1"/> : <Icon />}
-            <h4 className={`text-sm ${
+        <h4 className={`text-sm ${
           feed && "hidden lg:flex justify-center w-full mx-auto"
         }`}>{text}</h4>
+
+        {active && (
+        <span className="hidden lg:inline-flex h-0.5 w-[calc(100%+20px)] bg-black dark:bg-white rounded-t-full" />
+      )}
+
         </div>
     );
 }
