2d6f725c5306795438a992a5010ad1d6d322569e
Stefano Sossi <stefano.sossi.tx@gmail.com>
2022-06-15 20:00:46 -0400

    [DAT-7-5] Add providers to button

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,20 @@
+function Home({ providers }) {
+    //Mergue comflict to resolve
+    {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <div className="pl-4">
+            <button
+              className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in
+            </button>
+          </div>
+        </div>
+      ))}
+    ///--------------------
+}
+
 export async function getServerSideProps(context) {
     const providers = await getProviders();
   
