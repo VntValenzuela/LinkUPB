b47ab92bd293c7d9c93916e2cdab46c9bca33fc0
paoli99 <pao_anahi_8@hotmail.com>
2022-06-15 21:09:50 -0400

    [DAT-10-6] added dark mode theme

--- components/Header.js
+++ components/Header.js
@@ -19,34 +19,58 @@ function Header(){
     
     return(
         <header>
-            <div className="flex items-center space-x-2 w-full max-w-xs">
-            <Image src="https://rb.gy/bizvqj" width={45} height={45} />
-            
-            <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
-                <SearchRoundedIcon/>
-                <input type="text" placeholder="Search" className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"/>
-            </div> 
+        <div className="flex items-center space-x-2 w-full max-w-xs">
+        {mounted && (
+          <>
+            {resolvedTheme === "dark" ? (
+              <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+            ) : (
+              <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
+            )}
+          </>
+        )}
+        <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
+          <SearchRoundedIcon />
+          <input
+            type="text"
+            placeholder="Search"
+            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
+          />
+        </div>
+        </div>
 
-            </div>
+            
+            
+        <div className="flex items-center space-x-6">
+        <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+        <HeaderLink Icon={GroupIcon} text="My Network" feed />
+        <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
+        <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+        <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
+        <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
+        <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
 
-            
-            
-            <div className="flex items-center space-x-6">
-            <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
-            <HeaderLink Icon={GroupIcon} text="My Network" feed />
-            <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
-            <HeaderLink Icon={ChatIcon} text="Messaging" feed />
-            <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
-            <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
-            <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
-            
-            {/* Dark mode */}
-            <div className="bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer flex-shrink-0 relative">
+        {/* Dark mode toggle */}
+        {mounted && (
+          <div
+            className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer flex-shrink-0 relative ${
+              resolvedTheme === "dark" ? "justify-end" : "justify-start"
+            }`}
+            onClick={() =>
+              setTheme(resolvedTheme === "dark" ? "light" : "dark")
+            }
+          >
             <span className="absolute left-0">🌜</span>
-            <span className="absolute right-0.5">🌞</span>
-            </div>
+            <motion.div
+              className="w-5 h-5 bg-white rounded-full z-40"
+              layout
+              transition={spring}
+            />
 
-            </div>
+            <span className="absolute right-0.5">🌞</span>
+          </div>
+        )}
+      </div>
         </header>
     )
 }
