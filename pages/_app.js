4472c514f2957baecbe951e06468d7e585c6e9cc
Stefano Sossi <stefano.sossi.tx@gmail.com>
2022-06-15 20:21:37 -0400

    [DAT-10-8] Add SessionProvider component to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </SessionProvider>
 }
 
 export default MyApp
