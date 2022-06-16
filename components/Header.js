bc1a42296f1f5e027922ef95ec3c143e4bf0275b
paoli99 <pao_anahi_8@hotmail.com>
2022-06-15 21:15:05 -0400

    [DAT-10-6] fixed missing library

--- components/Header.js
+++ components/Header.js
@@ -43,35 +43,21 @@ function Header(){
             
             
         <div className="flex items-center space-x-6">
-        <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
-        <HeaderLink Icon={GroupIcon} text="My Network" feed />
-        <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
-        <HeaderLink Icon={ChatIcon} text="Messaging" feed />
-        <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
-        <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
-        <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
-
-        {/* Dark mode toggle */}
-        {mounted && (
-          <div
-            className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer flex-shrink-0 relative ${
-              resolvedTheme === "dark" ? "justify-end" : "justify-start"
-            }`}
-            onClick={() =>
-              setTheme(resolvedTheme === "dark" ? "light" : "dark")
-            }
-          >
+            <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+            <HeaderLink Icon={GroupIcon} text="My Network" feed />
+            <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
+            <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+            <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
+            <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
+            <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
+            
+            {/* Dark mode */}
+            <div className="bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer flex-shrink-0 relative">
             <span className="absolute left-0">🌜</span>
-            <motion.div
-              className="w-5 h-5 bg-white rounded-full z-40"
-              layout
-              transition={spring}
-            />
-
             <span className="absolute right-0.5">🌞</span>
-          </div>
-        )}
-      </div>
+            </div>
+
+            </div>
         </header>
     )
 }
