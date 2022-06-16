4542f576cbf6f24df2f9b27b4e3f8590fd5caaec
Alejandra Quelali <alejandra.day.123@gmail.com>
2022-06-16 00:54:58 -0400

    [DAT-10-7]Added post Viewer and style line.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,20 +4,33 @@ function Sidebar()
 {
     return (
         <div className="space-y-2 min-w-max max-w-lg">
-        <div className="bg-white dark:bg-[#1D2226] rounded-lg overflow-hidden relative flex flex-col items-center text-center border border-gray-300 dark:border-none">
-            <div className="relative w-full h-14">
-                <Image src="https://rb.gy/i26zak" layout="fill" priority/>
+            `<div className="bg-white dark:bg-[#1D2226] rounded-lg overflow-hidden relative flex flex-col items-center text-center border border-gray-300 dark:border-none">
+                <div className="relative w-full h-14">
+                    <Image src="https://rb.gy/i26zak" layout="fill" priority/>
+                </div>
+            <Avatar 
+            // onClick={signOut}
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-q3WWsLEVdDsXEddGHxpoE00Y2knvcEJ5pAmjsoUM2DFPFxfZO7_Y2GjBunSUmlbJds&usqp=CAU"
+                classname="!h-14 !w-14 !border-2 !absolute !top-4 !cursor-pointer"    
+            />
             </div>
-        <Avatar 
-           // onClick={signOut}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-q3WWsLEVdDsXEddGHxpoE00Y2knvcEJ5pAmjsoUM2DFPFxfZO7_Y2GjBunSUmlbJds&usqp=CAU"
-            classname="!h-14 !w-14 !border-2 !absolute !top-4 !cursor-pointer"    
-        />
-        </div>
-        <div>
-            <h4>Carolina Quiroga</h4>
-            <p>carolinaquiroga@upb.edu</p>
-        </div>
+            <div className="mt-5 py-4 spay-x-0.5">
+                <h4 className="hover:underline decoration-blue-700 underline-offset-1 cursor-pointer">
+                    Carolina Quiroga</h4>
+                <p className="text-black/60 dark:text-white/75 text-sm">carolinaquiroga@upb.edu</p>
+            </div>
+            <div className="hidden md:inline text-left dark:text-white/75 text-sm">
+                <div className="font-medium sidebarButton space-y-0.5">
+                    <div className="flex justify-between space-x-2">
+                        <h4>Who viewed you profile</h4>
+                        <span className="text-blue-500">450</span>
+                    </div>
+                    <div className="flex justify-between space-x-2s">
+                        <h4>Views of your post</h4>
+                        <span className="text-blue-500">130</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
