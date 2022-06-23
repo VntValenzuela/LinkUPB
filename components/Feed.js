7f4bbb23a14c7dee0958c63fc9410c3bf6e9ee02
Stefano Sossi <stefano.sossi.tx@gmail.com>
2022-06-23 15:18:08 -0400

    [DAT-10-10] Fix error on imports

--- components/Feed.js
+++ components/Feed.js
@@ -2,6 +2,9 @@ import Input from "./Input"
 import Post from "./Post";
 
 function Feed({posts}){
+    const [realtimePosts, setRealtimePosts] = useState([]);
+    const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+    const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
     return (
         <div className="space-y-6 pb-24 max-w-lg">
         <Input />
--- components/Form.js
+++ components/Form.js
@@ -3,6 +3,8 @@ import { useState } from "react"
 function Form(){
     const [input, setInput] = useState("");
     const [photoUrl, setPhotoUrl] = useState("");
+    const { data: session } = useSession();
+    const [modalOpen, setModalOpen] = useRecoilState(modalState);
     const [handlePost, setHandlePost] = useRecoilState(handlePostState);
     const uploadPost = async(e) => {
         e.preventDefault();
