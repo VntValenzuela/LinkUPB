175a3c0462c67dfac58095174bf9d8976f545d8b
paoli99 <pao_anahi_8@hotmail.com>
2022-06-23 18:35:57 -0400

    [DAT-10-9] added avatar to posts

--- components/Post.js
+++ components/Post.js
@@ -1,3 +1,5 @@
+import { Avatar } from "@mui/material";
+
 function Post({post, modalPost}){
     return (
         <div className={`bg-white dark:bg-[#1D2226] ${
@@ -5,6 +7,10 @@ function Post({post, modalPost}){
           } space-y-2 py-2.5 border border-gray-300 dark:border-none`}>
             <img src={post.photoUrl} alt=""></img>
             <p>{post.input}</p>
+
+            <div className="flex items-center px-2.5 cursor-pointer">
+                <Avatar src={post.userImg} className="!h-10 !w-10 cursor-pointer"></Avatar>
+            </div>
         </div>
     );
 }
