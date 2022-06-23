78e4dd04eb1c70557a5687c066d37aa08468d5ef
paoli99 <pao_anahi_8@hotmail.com>
2022-06-23 18:41:22 -0400

    [DAT-10-9] added icons to posts

--- components/Post.js
+++ components/Post.js
@@ -10,6 +10,25 @@ function Post({post, modalPost}){
 
             <div className="flex items-center px-2.5 cursor-pointer">
                 <Avatar src={post.userImg} className="!h-10 !w-10 cursor-pointer"></Avatar>
+                <div className="mr-auto ml-2 leading-none">
+                    <h6 className="font-medium hover:text-blue-500 hover:underline">
+                        {post.username}
+                    </h6>
+                    <p className="text-sm dark:text-white/75 opacity-80">{post.email}</p>
+                    <TimeAgo
+                        datetime={post.createdAt}
+                        className="text-xs dark:text-white/75 opacity-80"
+                    />
+                </div>
+                {modalPost ? (
+                <IconButton onClick={() => setModalOpen(false)}>
+                    <CloseRoundedIcon className="dark:text-white/75 h-7 w-7" />
+                </IconButton>
+                ) : (
+                <IconButton>
+                    <MoreHorizRoundedIcon className="dark:text-white/75 h-7 w-7" />
+                </IconButton>
+                )}
             </div>
         </div>
     );
