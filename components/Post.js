b3af34c3d5b4b8b36f2e055cfd57a0f54003931d
paoli99 <pao_anahi_8@hotmail.com>
2022-06-23 18:48:31 -0400

    [DAT-10-9] added a show more option for posts

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,25 @@
-import { Avatar,IconButton  } from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
+import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import ThumbUpOffAltOutlinedIcon from "@mui/icons-material/ThumbUpOffAltOutlined";
+import ThumbUpOffAltRoundedIcon from "@mui/icons-material/ThumbUpOffAltRounded";
 import { useRecoilState } from "recoil";
-import { modalState } from "../atoms/modalAtom";
+import { handlePostState, getPostState } from "../atoms/postAtom";
+import { useState } from "react";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import ReplyRoundedIcon from "@mui/icons-material/ReplyRounded";
+import CommentOutlinedIcon from "@mui/icons-material/CommentOutlined";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
+import TimeAgo from "timeago-react";
+import { useSession } from "next-auth/react";
 
 function Post({post, modalPost}){
-    const [modalOpen, setModalOpen] = useRecoilState(modalState);
+    const [modalOpen, setModalOpen] = useRecoilState(modalState)
+    const [showInput, setShowInput] = useState(false);
+
+    const truncate = (string, n) =>
+    string?.length > n ? string.substr(0, n - 1) + "...see more" : string;
+    
     return (
         <div className={`bg-white dark:bg-[#1D2226] ${
             modalPost ? "rounded-r-lg" : "rounded-lg"
@@ -33,6 +49,19 @@ function Post({post, modalPost}){
                 </IconButton>
                 )}
             </div>
+
+            {post.input && (
+                <div className="px-2.5 break-all md:break-normal">
+                {modalPost || showInput ? (
+                    <p onClick={() => setShowInput(false)}>{post.input}</p>
+                ) : (
+                    <p onClick={() => setShowInput(true)}>
+                    {truncate(post.input, 150)}
+                    </p>
+                )}
+                </div>
+            )}
+
         </div>
     );
 }
